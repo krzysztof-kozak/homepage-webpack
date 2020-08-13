718fa075ccf169b57e68ab4211a855cbd5a1459d
Bartosz Małecki <bartoszmalecki90@gmail.com>
2020-08-13 22:34:52 +0200

    week8 content done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import '../scss/main.scss';
 
+import moment from 'moment';
+
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
@@ -30,3 +32,5 @@ hamburger.addEventListener('click', () => {
   const nav = document.querySelector('.navigation--js');
   nav.classList.toggle('navigation--open');
 })
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
